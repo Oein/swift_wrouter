37ad20d74f307be31dfb9ecae270bf436d4e3fd3
Oein <oein0219@gmail.com>
2025-01-26 00:46:47 +0900

    Initial Commit

---
+++ weblikerouter/weblikerouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  weblikerouterApp.swift
+//  weblikerouter
+//
+//  Created by oein on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct weblikerouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
