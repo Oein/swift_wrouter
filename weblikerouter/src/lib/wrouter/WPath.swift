aa097e2dc2ea01ad0126b199ffd3f14f4eb14801
Oein <oein0219@gmail.com>
2025-01-26 07:18:40 +0900

    fix WPath bug not forwarded after backward

--- weblikerouter/src/lib/wrouter/WPath.swift
+++ weblikerouter/src/lib/wrouter/WPath.swift
@@ -1,9 +1,10 @@
 //
-//  WRouter_PathManager.swift
-//  weblikerouter
+//  WPath 2.swift
+//  musicplus
 //
 //  Created by oein on 1/26/25.
 //
+
 
 import Foundation
 import SwiftUI
@@ -14,22 +15,22 @@ import SwiftUI
     
     public var path = "::blank";
     public var queryparm = "";
-    public var background: Color? = nil;
+    public var background: Color? = Color.backgroundMain;
     
-    private var viewhistory = WRouter_Stack<WRouter_URL>();
-    private var backward_history = WRouter_Stack<WRouter_URL>();
+    private var history = WRouter_Stack<WRouter_URL>();
+    private var forward_stack = WRouter_Stack<WRouter_URL>();
     
     func goto(path: String, qparm: String?) {
-        self.viewhistory.push(WRouter_URL(path: self.path, qparm: self.queryparm));
-        self.backward_history.clear();
+        self.history.push(WRouter_URL(path: self.path, qparm: self.queryparm));
+        self.forward_stack.clear()
         
         self.path = path;
         self.queryparm = qparm ?? "";
     }
     
     func set_path(path: String, qparm: String?) {
-        self.viewhistory.clear();
-        self.backward_history.clear();
+        self.history.clear();
+        self.forward_stack.clear()
         
         self.path = path;
         self.queryparm = qparm ?? "";
@@ -41,27 +42,23 @@ import SwiftUI
     }
     
     func backward() {
-        if viewhistory.isEmpty {
+        if history.isEmpty {
             return;
         }
         
-        let lastPath = self.viewhistory.pop()!;
-        self.backward_history.push(WRouter_URL(path: self.path, qparm: self.queryparm));
+        let lastPath = self.history.pop()!;
+        self.forward_stack.push(WRouter_URL(path: self.path, qparm: self.queryparm));
         self.path = lastPath.path;
         self.queryparm = lastPath.qparm;
     }
     
-    func canForward() -> Bool {
-        return !self.backward_history.isEmpty;
-    }
-    
     func forward() {
-        if self.backward_history.isEmpty {
+        if self.forward_stack.isEmpty {
             return;
         }
         
-        let futurePath = self.backward_history.pop()!;
-        self.backward_history.push(WRouter_URL(path: self.path, qparm: self.queryparm));
+        let futurePath = self.forward_stack.pop()!;
+        self.history.push(WRouter_URL(path: self.path, qparm: self.queryparm));
         
         self.path = futurePath.path;
         self.queryparm = futurePath.qparm;
